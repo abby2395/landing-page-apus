2625fee0118458d79c62df933f50fba802ef7d23
asrivastavrangers <asrivastav@sandiego.edu>
2025-04-06 18:26:32 +0530

    Added services url

--- surveyApp/urls.py
+++ surveyApp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index, about
+from .views import index, about, services
 
 urlpatterns = [
     path('', index, name="Homepage"),
     path('about/', about, name="About"),
+    path('services/', services, name="Services"),
 ]
